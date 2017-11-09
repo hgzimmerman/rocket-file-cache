b9c9c175fc7c53ac7bc5c7bf85c58d0d7958c0b6
Henry Zimmerman <zimhen7@gmail.com>
2017-11-09 08:51:53 -0500

    add a builder

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,13 @@ use std::usize;
 use std::fmt;
 use std::sync::Arc;
 
+// TODO consider adding the priority score to this struct.
+// currently, when determining if a file can be added, the priority score must be calculated for
+// every file in the cache.
+// If every file had it pre-computed, either here, as part of the access count map, or as a new
+// hashmap, then this expensive operation wouldn't have to be performed.
+// That would take a O(n) operation on inserts, and make it a O(1) operation on gets and inserts,
+// which would lead to more consistent performance.
 /// The structure that represents a file in memory.
 /// Keeps a copy of the size of the file so the size can be used in calculating if it should be
 /// removed from the cache.
@@ -113,6 +120,81 @@ pub enum CacheInvalidationSuccess {
     InsertedFileIntoAvailableSpace,
 }
 
+pub enum CacheBuildError {
+    SizeLimitNotSet,
+    MinFileSizeIsLargerThanMaxFileSize
+}
+
+pub struct CacheBuilder {
+    size_limit: Option<usize>,
+    priority_function: Option<PriorityFunction>,
+    min_file_size: Option<usize>,
+    max_file_size: Option<usize>
+}
+
+impl CacheBuilder {
+    pub fn new() -> CacheBuilder {
+        CacheBuilder {
+            size_limit: None,
+            priority_function: None,
+            min_file_size: None,
+            max_file_size: None
+        }
+    }
+
+    /// Mandatory parameter, must be set.
+    /// Sets the maximum number of bytes the cache can hold.
+    pub fn size_limit_bytes(&mut self, size_limit_bytes: usize) {
+        self.size_limit = Some(size_limit_bytes);
+    }
+
+    /// Override the default priority function used for determining if the cache should hold a file.
+    /// By default a score is calculated using the square root of the size of a file, times the number
+    /// of times it was accessed.
+    /// Files with higher priority scores will be kept in the cache when files with lower scores are
+    /// added.
+    /// If there isn't room in the cache for two files, the one with the lower score will be removed /
+    /// won't be added.
+    ///
+    /// The priority function should be kept simple, as it is calculated on every file in the cache
+    /// every time a new file is attempted to be added.
+    pub fn priority_function(&mut self, priority_function: PriorityFunction) {
+        self.priority_function = Some(priority_function);
+    }
+
+    // TODO, implement min and max file sizes
+
+    pub fn build(self) -> Result<Cache, CacheBuildError> {
+        let size_limit = match self.size_limit {
+            Some(s) => s,
+            None => return Err(CacheBuildError::SizeLimitNotSet)
+        };
+
+        let priority_function: PriorityFunction = match self.priority_function {
+            Some(p) => p,
+            None => Cache::DEFAULT_PRIORITY_FUNCTION
+        };
+
+        if let Some(min_file_size) = self.min_file_size {
+            if let Some(max_file_size) = self.max_file_size {
+                if min_file_size > max_file_size {
+                    return Err(CacheBuildError::MinFileSizeIsLargerThanMaxFileSize)
+                }
+            }
+        }
+
+        Ok(
+            Cache {
+                size_limit,
+                priority_function,
+                file_map: HashMap::new(),
+                access_count_map: HashMap::new()
+            }
+        )
+
+    }
+}
+
 /// The Cache holds a set number of files.
 /// The Cache acts as a proxy to the filesystem.
 /// When a request for a file is made, the Cache checks to see if it has a copy of the file.
@@ -141,15 +223,6 @@ impl Cache {
         }
     }
 
-    /// Creates a new Cache with the given size limit and a specified priority function.
-    pub fn new_with_priority_function(size_limit: usize, priority_function: PriorityFunction) -> Cache {
-        Cache {
-            size_limit,
-            priority_function,
-            file_map: HashMap::new(),
-            access_count_map: HashMap::new(),
-        }
-    }
 
     /// Attempt to store a given file in the the cache.
     /// Storing will fail if the current files have more access attempts than the file being added.
