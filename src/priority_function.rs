f303e53eff9184b35fa921b51e6fa7d9ba1f1395
Henry Zimmerman <zimhen7@gmail.com>
2017-11-13 09:34:59 -0500

    added priority functions, fixed style for fn creation

--- src/priority_function.rs
+++ src/priority_function.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 /// Custom type of function that is used to determine how to add files to the cache.
 /// The first term will be assigned the access count of the file in question, while the second term will be assigned the size (in bytes) of the file in question.
 /// The result will represent the priority of the file to remain in or be added to the cache.
@@ -9,13 +11,38 @@ pub type PriorityFunction = fn(usize, usize) -> usize;
 
 /// The default priority function used for determining if a file should be in the cache
 /// This function takes the square root of the size of the file times the number of times it has been accessed.
-fn balanced_priority(access_count: usize, size: usize) -> usize {
-    ((size as f64).sqrt() as usize) * access_count
-}
-pub const DEFAULT_PRIORITY_FUNCTION: PriorityFunction = balanced_priority;
+/// This should give some priority to bigger files, while still allowing som smaller files to enter the cache.
+pub const DEFAULT_PRIORITY_FUNCTION: PriorityFunction = |access_count: usize, size: usize| {
+ ((size as f64).sqrt() as usize) * access_count
+};
+
+/// Priority is calculated as size * access count.
+pub const NORMAL_PRIORITY_FUNCTION: PriorityFunction = |access_count: usize, size: usize| {
+    size * access_count
+};
 
 /// This priority function will value files in the cache based solely on the number of times the file is accessed.
-fn access_priority(access_count: usize, _: usize) -> usize {
+pub const ACCESS_PRIORITY_FUNCTION: PriorityFunction = |access_count: usize, _: usize| {
     access_count
-}
-pub const ACCESS_PRIORITY_FUNCTION: PriorityFunction = access_priority;
+};
+
+
+/// Favor small files.
+/// The smaller the file, the higher priority it will have.
+/// Does not take into account the number of accesses the file has.
+pub const SMALL_FILES_PRIORITY_FUNCTION: PriorityFunction = |_: usize, size: usize| {
+    if size == 0 {
+        return 0 // don't give any priority to completely empty files.
+    }
+    usize::MAX / size
+};
+
+/// Favor small files.
+/// The smaller the file, the higher priority it will have.
+/// Does take into account the number of accesses the file has.
+pub const SMALL_FILES_ACCESS_PRIORITY_FUNCTION: PriorityFunction = |access_count: usize, size: usize| {
+    if size == 0 {
+        return 0 // don't give any priority to completely empty files.
+    }
+    (usize::MAX / size) * access_count
+};
