1c8e2d8cb490b80c26efe6fedacfd1108c41120d
Henry Zimmerman <zimhen7@gmail.com>
2017-11-15 09:59:39 -0500

    added a cache pool example

---
+++ examples/cache_pool/src/main.rs
@@ -0,0 +1,70 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_file_cache;
+extern crate rand;
+
+use rocket_file_cache::{Cache, ResponderFile};
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+use std::path::{Path, PathBuf};
+use rocket::State;
+use std::sync::Arc;
+use rand::{thread_rng, Rng};
+
+#[get("/<file..>")]
+fn files(file: PathBuf, cache_pool: State<Pool> ) -> Option<ResponderFile> {
+    let path: PathBuf = Path::new("www/").join(file).to_owned();
+
+    cache_pool.try_get().unwrap().get(&path)
+}
+
+
+fn main() {
+
+    let pool : Pool =  Pool::new(4, || Cache::new(1024 * 1024 * 50));
+    rocket::ignite()
+        .manage(pool)
+        .mount("/", routes![files])
+        .launch();
+}
+
+
+/// A pool that holds many caches.
+/// The caches held by the pool are NOT guaranteed to have the same files in them.
+/// Assuming random access of caches from the pool, and a large enough sample, the content's of the
+/// owned caches should trend towards being the same.
+#[derive(Clone, Debug)]
+struct Pool {
+    caches: Vec<Arc<Mutex<Cache>>>
+}
+
+impl Pool {
+    fn new(number_of_entries: usize, cache_creation_function: fn() -> Cache) -> Pool {
+        let mut caches: Vec<Arc<Mutex<Cache>>> = vec!();
+
+        for _ in 0..number_of_entries {
+            caches.push(Arc::new(Mutex::new(cache_creation_function())))
+        }
+        Pool {
+            caches
+        }
+    }
+
+
+    fn try_get<'a>(&'a self) -> Option<MutexGuard<'a, Cache>> {
+
+        // Randomize the range that can be accessed
+        let mut range: Vec<usize> = (0..self.caches.len()).collect();
+        thread_rng().shuffle(range.as_mut_slice());
+
+        for i in range.into_iter() {
+            if let Some(c) = self.caches[i].try_lock().ok() {
+                println!("Locked cache {}", i);
+                return Some(c) // Found a cache that wasn't locked
+            }
+        }
+        None // All caches are occupied
+    }
+}
